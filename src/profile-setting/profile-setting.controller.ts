aabb63938ebee8a29f127eb6c4b65f451bb970a4
zidanindratama <zidanindratama31@gmail.com>
2025-04-17 11:21:35 +0700

    feat: menambahkan roles ke controller

--- src/profile-setting/profile-setting.controller.ts
+++ src/profile-setting/profile-setting.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Patch, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { ProfileSettingService } from './profile-setting.service';
 import { ProfileSettingUpdateDto } from './dtos/profile-setting-update.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('api/profile-setting')
 export class ProfileSettingController {
@@ -12,6 +13,7 @@ export class ProfileSettingController {
     return req.user;
   }
 
+  @Roles('MAHASISWA')
   @Patch('update')
   updateMahasiswa(@Req() req: Request, @Body() body: ProfileSettingUpdateDto) {
     const user = req.user;
